6e1ab8185b52f082c8143a65e4afe8d82ad9fc47
= <rejanchhantyal122@gmail.com>
2026-05-16 10:51:23 +0545

    feat(portfolio): finalize personal website
    - Completed design and layout of portfolio page
    - Added sections for projects, skills, and contact
    - Polished styling and integrated backend/frontend features

--- js/script.js
+++ js/script.js
@@ -12,3 +12,72 @@ navOptions.forEach(option => {
     });
 });
 
+
+//for date select effect
+const dates = document.querySelectorAll('.dates li');
+
+dates[0].classList.add('bgc');
+
+dates.forEach(date => {
+    date.addEventListener('click', () => {
+        dates.forEach(dt => dt.classList.remove('bgc'));
+
+        date.classList.add('bgc');
+    });
+});
+
+
+//to show the conetent according to the nav
+const welCon = document.getElementById("welcome-content");
+const porCon = document.getElementById("portfolio-content");
+const forCon = document.getElementById("form-content");
+
+function showSection(id){
+
+    welCon.style.display = "none";
+    porCon.style.display = "none";
+    forCon.style.display = "none";
+
+    document.getElementById(id).style.display = "block";
+}
+
+// search bar pop up open
+function searchOpen(){
+    document.getElementById("search-pop-up-wrapper-id").classList.add('pop');
+    document.body.classList.add('stopscrolling');
+}
+
+//search bar pop up close
+function searchClose(){
+    document.getElementById("searchbar-input").value = "";
+    document.getElementById("search-pop-up-wrapper-id").classList.remove('pop');
+    document.body.classList.remove('stopscrolling');
+}
+
+//for light mode
+const darkMode = document.getElementById("mode-theme");
+const lightMode = document.getElementById("dark-mode-theme");
+
+function enableLightMode(){
+    document.body.classList.add('lightmode');
+    darkMode.style.display = "none";
+
+    lightMode.style.display = "flex";
+}
+function disableLightMode(){
+    document.body.classList.remove('lightmode');
+    darkMode.style.display = "flex";
+
+    lightMode.style.display = "none";
+}
+
+//functions to  open and close the side menu bar
+function openMenu(){
+    document.body.classList.add('stopscrolling');
+    document.getElementById("menu").classList.add('open');
+}
+
+function closeMenu(){
+    document.body.classList.remove('stopscrolling');
+    document.getElementById("menu").classList.remove('open');
+}
